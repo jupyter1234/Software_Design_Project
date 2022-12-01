bcf5e0175af519aa0c480a24e7e5762167c8282e
Junseok Park <95912522+Lucerna00@users.noreply.github.com>
2022-12-02 01:05:36 +0900

    SideMenuList 클래스

---
+++ Menu/SideMenuList.java
@@ -0,0 +1,11 @@
+public class SideMenuList extends MenuList {
+    private static SideMenuList instance = null;
+
+    private SideMenuList(){}
+
+    public static SideMenuList getInstance(){
+        if(instance == null)
+            instance = new SideMenuList();
+        return instance;
+    }
+}
