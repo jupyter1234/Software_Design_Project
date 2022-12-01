8e5ca354049215515e83ebbe1d52050c459fa3c8
Junseok Park <95912522+Lucerna00@users.noreply.github.com>
2022-12-02 01:05:08 +0900

    MainMenuList 클래스

---
+++ Menu/MainMenuList.java
@@ -0,0 +1,11 @@
+public class MainMenuList extends MenuList {
+    private static MainMenuList instance = null;
+
+    private MainMenuList(){}
+
+    public static MainMenuList getInstance(){
+        if(instance == null)
+            instance = new MainMenuList();
+        return instance;
+    }
+}
